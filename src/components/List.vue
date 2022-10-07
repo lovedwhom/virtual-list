bb141a051542517dbc3f3b730e82a2af504b51a3
hebin <hebin@kuangheng.com>
2022-10-07 23:44:39 +0800

    build: 初始化项目

---
+++ src/components/List.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="(item,index) in listData" :key="index">
+                {{ index }} 
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+// interface ListData {
+
+// }
+export default defineComponent({
+    setup() {
+        let listData:Array<any> = new Array(10000)
+            
+            return {
+                listData
+            }
+    },
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
+
